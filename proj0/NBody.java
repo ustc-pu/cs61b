18c807239a5bb5702e7dd5565d3fb8e8e15d2904
Pu Cheng <1483030518@qq.com>
2019-04-14 12:45:58 +0800

    finish the Planet method in NBody

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -6,12 +6,33 @@ public class NBody {
 		return radius;
 	}
 
-	public static double[] readPlanets(String path) {
+	/**
+	We use Planet[] as the return value.
+	Each element of the array is a Planet object. 
+	Each object has six variables which are exactly in the file.
+	We can use the constructor of Planet.
+	*/
+
+	public static Planet[] readPlanets(String path) {
 		In in = new In(path);
+		int num = in.readInt();
+		double radius = in.readDouble();
+		Planet[] planets = new Planet[5];
+		for(int i=0; i<5; i++) {
+			Double xxPos = in.readDouble();
+			Double yyPos = in.readDouble();
+			Double xxVel = in.readDouble();
+			Double yyVel = in.readDouble();
+			Double mass = in.readDouble();
+			String imgFileName = in.readString();
+			planets[i] = new Planet(xxPos, yyPos, xxVel, 
+				yyVel, mass, imgFileName);
+		}
+		return planets;		
 	}
 
 	public static void main(String[] args) {
-		Double ra = NBody.readRadius("/data/planets.txt");
-		System.out.println(ra);
+		//Double ra = NBody.readRadius("/data/planets.txt");
+		//System.out.println(ra);
 	}
 }
