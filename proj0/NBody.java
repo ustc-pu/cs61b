1474d113a57a9091698e5a75e38274d186c4324e
Pu Cheng <1483030518@qq.com>
2019-04-15 15:37:25 +0800

    finish drawing star and planets in NBody.java in proj0

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -35,12 +35,20 @@ public class NBody {
 		Double T = Double.parseDouble(args[0]);
 		Double dt = Double.parseDouble(args[1]);
 		String filename = args[2];
-		System.out.println(T);
-		System.out.println(filename);
+		//System.out.println(T);
+		//System.out.println(filename);
 
-		StdDraw.setScale(-100, 100);
+		//Please set the scale to the radius of the universe
+		Double radius = NBody.readRadius(filename);
+		StdDraw.setScale(0-radius, radius);
 		StdDraw.clear();
 		StdDraw.picture(0, 0, "/images/starfield.jpg");
 		StdDraw.show();
+
+		Planet[] p = NBody.readPlanets(filename);
+		for(int i=0; i<5; i++) {
+			System.out.println(p[i].imgFileName);
+			p[i].draw();
+		}		
 	}
 }
