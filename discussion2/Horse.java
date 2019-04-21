1d471322d95905e1fce39e92643afc5bebd09f7a
Pu Cheng <1483030518@qq.com>
2019-04-21 22:22:04 +0800

    add some notes

--- discussion2/Horse.java
+++ discussion2/Horse.java
@@ -1,3 +1,12 @@
+/**
+In java, there are eight kinds of primitive types. 
+The rest is called reference type. For example, the String and the Horse here.
+The usage of key word this: 
+1) in constructor, this refers to the new object;
+2) in normal method, this refers to the object which calls the method.
+
+*/
+
 public class Horse {
 	Horse same;
 	String jimmy;
@@ -7,12 +16,19 @@ public class Horse {
 	}
 
 	public Horse same(Horse horse) {
+		/**
+		the parameter passed is horse (line 38)
+		this == cult, which calls the method
+		the this.same attribute of cult is not null
+		and same here refers to cult, which is a circle
+		same.same also refers to cult.
+		*/
 		if (same != null) {  
 			System.out.println("what is the address of this same? "+same);
 			System.out.println(same.jimmy);
-			Horse same = horse; 
+			Horse same = horse; // the object same refers to horse 38
 			System.out.println("the address of the local same "+same);
-			same.same = horse;  
+			same.same = horse;  // horse also becomes a circle
 			same = horse.same;  
 		}
 		System.out.println("again, the address of the same is?? " + same);
@@ -25,8 +41,9 @@ public class Horse {
 		System.out.println("the address of horse "+horse);
 		Horse cult = new Horse("horsed");
 		System.out.println("the address of cult "+cult);
-		cult.same = cult;
-		cult = cult.same(horse);
+		cult.same = cult; // now we have a circle and same refer to cult.
+		System.out.println("the address of cult.same "+cult.same);
+		cult = cult.same(horse); // here cult calls the same method
 		System.out.println(cult.jimmy);
 		System.out.println(horse.jimmy);
 	}
