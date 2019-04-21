6b8d12f6fd13f8c82ea83f5fb929c009050d76e5
Pu Cheng <1483030518@qq.com>
2019-04-21 21:40:45 +0800

    add some print lines to help undersatnd

--- discussion2/Horse.java
+++ discussion2/Horse.java
@@ -7,17 +7,24 @@ public class Horse {
 	}
 
 	public Horse same(Horse horse) {
-		if (same != null) {
-			Horse same = horse;
-			same.same = horse;
-			same = horse.same;
+		if (same != null) {  
+			System.out.println("what is the address of this same? "+same);
+			System.out.println(same.jimmy);
+			Horse same = horse; 
+			System.out.println("the address of the local same "+same);
+			same.same = horse;  
+			same = horse.same;  
 		}
-		return same.same;
+		System.out.println("again, the address of the same is?? " + same);
+		System.out.println("and the address of same.same is "+same.same);
+		return same.same;  
 	}
 
 	public static void main(String[] args) {
 		Horse horse = new Horse("youve been");
+		System.out.println("the address of horse "+horse);
 		Horse cult = new Horse("horsed");
+		System.out.println("the address of cult "+cult);
 		cult.same = cult;
 		cult = cult.same(horse);
 		System.out.println(cult.jimmy);
