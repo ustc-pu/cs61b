8ad745f868d72ea40cae468197a2e844e897ac5c
Pu Cheng <1483030518@qq.com>
2019-05-19 16:35:55 +0800

    add comments about the bug of Integer

--- lab3/Flik/Flik.java
+++ lab3/Flik/Flik.java
@@ -2,5 +2,11 @@
 public class Flik {
     public static boolean isSameNumber(Integer a, Integer b) {
         return a == b;
+        /*
+        * when Integer is a constant, it has been cashed between -128 and 127.
+        * If exceeding this range, we need to new Integer object say Integer(128)
+        * which will have different address.
+        * thus it returns false outside this range.
+        * */
     }
 }
