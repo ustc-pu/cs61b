94401c99acaaf56a3bd6b579d577184f5d466ae1
Pu Cheng <1483030518@qq.com>
2019-06-19 22:17:57 +0800

    finish isPalindrome recursively

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,12 +1,19 @@
 public class Palindrome {
 
     public Deque<Character> wordToDeque(String word) {
-//        LinkedListDeque<String> d = new LinkedListDeque();
+//      the signature of an interface object Interface obj = new Class
         Deque<Character> d = new LinkedListDeque();
         for(int i = 0; i < word.length(); i++) {
             char temp = word.charAt(i);
             d.addLast(temp);
         }
         return d;
+    }
+
+    public boolean isPalindrome(String word) {
+        Palindrome p = new Palindrome();
+        Deque d = p.wordToDeque(word);
+        Boolean b = d.isPalindrome(d);
+        return b;
     }
 }
