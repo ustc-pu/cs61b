9a8c766a04032a0c87db3ea9f09dfbedea49fbea
Pu Cheng <1483030518@qq.com>
2019-06-25 21:25:46 +0800

    add OffByN file

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -1,2 +1,16 @@
-public class OffByN {
+public class OffByN implements CharacterComparator {
+
+    int diffExpected;
+    @Override
+    public boolean equalChars(char x, char y) {
+        int diffActual = x - y;
+        if (diffActual == diffExpected || diffActual == -diffExpected) {
+            return true;
+        }
+        return false;
+    }
+
+    public OffByN(int N) {
+        this.diffExpected = N;
+    }
 }
