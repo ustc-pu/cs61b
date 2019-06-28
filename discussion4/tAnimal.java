7bfd315625af093322a6f7d6ed1faf66c20d9f5e
Pu Cheng <1483030518@qq.com>
2019-06-28 20:39:14 +0800

    delete disc4 files

--- discussion4/tAnimal.java
+++
@@ -1,7 +0,0 @@
-public class tAnimal {
-    public static void main(String[] args) {
-        Animal a1 = new Animal("Pinky", 3);
-        a1.makeNoise();
-        a1.greet();
-    }
-}
--- discussion4/testAnimal.java
+++
@@ -1,7 +0,0 @@
-public class testAnimal {
-    public static void main(String[] args) {
-        Animal a1 = new Cat();
-        a1.makeNoise();
-        a1.greet();
-    }
-}
