8c45ca830ebc51a92389853870dc4bea4d43c043
Pu Cheng <1483030518@qq.com>
2019-06-28 21:35:08 +0800

    add testCat.java file

---
+++ discussion4/testCat.java
@@ -0,0 +1,9 @@
+public class testCat {
+    public static void main(String[] args) {
+        Animal a1 = new Animal("BoBo", 5);
+        a1.greet();
+
+        Animal a2 = new Cat("Pinky", 2);
+        a2.greet();
+    }
+}
